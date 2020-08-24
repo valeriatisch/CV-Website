9345f7ca4d2f7f0e9c42830277dbb266867ef4f9
Valeria Tisch <valeria_nov@yahoo.de>
2020-08-24 22:23:46 +0200

    first commit - responsiveness and half of structure done

---
+++ js/main.js
@@ -0,0 +1,34 @@
+console.log('It works')
+
+$(document).ready(function () {
+    $('.sendbutton').click(function (event) {
+      console.log('Clicked button')
+
+        const email = $('.email').val();
+        const subject = $('.subject').val();
+        const message = $('.message').val()
+        const statusElm = $('.status');
+        statusElm.empty()
+
+        if(email.length >= 5 && email.includes('@') && email.includes('.')) {
+            console.log("EMail is valid.")
+        } else {
+            event.preventDefault()
+            statusElm.append('<div>Etwas stimmt mit deiner E-Mail-Adresse nicht! :(</div>')
+        }
+
+        if(subject.length >= 2) {
+            console.log("Subject is valid")
+        } else {
+            event.preventDefault()
+            statusElm.append('<div>Dein Betreff ist leider zu kurz :(</div>')
+        }
+
+        if(message.length >= 15) {
+            console.log("Message is valid")
+        } else {
+            event.preventDefault()
+            statusElm.append('<div>Deine Nachricht ist leider zu kurz :(</div>')
+        }
+    })
+})
